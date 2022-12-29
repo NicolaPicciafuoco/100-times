f622acd763be718ca16d37e853b4ffde59f51283
nicopiccia <nicola.picciafuoco@gmail.com>
2022-12-29 11:44:14 +0100

    for costructure

--- 100 time.py
+++ 100 time.py
@@ -1,1 +1,5 @@
-fwef
+
+#this programs write something, i try to write in wathsapp
+
+import time
+import pyautoguy
---
+++ main.py
@@ -0,0 +1,12 @@
+
+import time
+import pyautogui as pg
+
+print(" program in 5 second ")
+time.sleep(15)
+
+for i in range(100):
+    pg.write(" ti amo un botto")
+    time.sleep(0.5)
+    pg.press("enter")
+
